0af852b0d02591f48f514ca8cd17790ce13f78b9
Zahid Hasan <zahidhasanshellbeehaken@gmail.com>
2026-01-29 14:59:32 +0600

    navbar  icons added

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,8 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
- 
-import CounterHomeScreen from '@/features/counters/screens/CounterHomeScreen';
 import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
+import CounterHomeScreen from '@/features/counters/screens/CounterHomeScreen';   
 const Tab = createBottomTabNavigator();
 
 function Placeholder({ title }) {
@@ -15,7 +15,28 @@ function Placeholder({ title }) {
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Counters') {
+            iconName = focused ? 'add-circle' : 'add-circle-outline';
+          } else if (route.name === 'Analytics') {
+            iconName = focused ? 'bar-chart' : 'bar-chart-outline';
+          } else if (route.name === 'Goals') {
+            iconName = focused ? 'flag' : 'flag-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#000',
+        tabBarInactiveTintColor: 'gray',
+        headerShown: false,
+      })}
+    >
       <Tab.Screen name="Counters" component={CounterHomeScreen} />
       <Tab.Screen name="Analytics" children={() => <Placeholder title="Analytics" />} />
       <Tab.Screen name="Goals" children={() => <Placeholder title="Goals" />} />
