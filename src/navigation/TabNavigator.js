eded9412ae3620653e1f0337678850b00989494e
Zahid Hasan <zahidhasanshellbeehaken@gmail.com>
2026-02-02 10:53:43 +0600

    updated the code according to doc

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,11 +1,10 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStaticNavigation } from "@react-navigation/native";
 import { Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-
+ 
 import CounterHomeScreen from "@/features/counters/screens/CounterHomeScreen";
-
-const Tab = createBottomTabNavigator();
-
+ 
 export const TabOption = Object.freeze({
   COUNTER: "Counters",
   ANALYTICS: "Analytics",
@@ -13,61 +12,63 @@ export const TabOption = Object.freeze({
   SETTINGS: "Settings",
 });
 
-function getTabIcon(routeName = " ", focused = false) {
+ 
+function getTabIcon(routeName, focused) {
   switch (routeName) {
     case TabOption.COUNTER:
       return focused ? "add-circle" : "add-circle-outline";
-
     case TabOption.ANALYTICS:
       return focused ? "bar-chart" : "bar-chart-outline";
-
     case TabOption.GOALS:
       return focused ? "flag" : "flag-outline";
-
     case TabOption.SETTINGS:
       return focused ? "settings" : "settings-outline";
-
     default:
       return "ellipse-outline";
   }
 }
 
-function createScreenOptions({ route = {} }) {
-  return {
-    tabBarIcon: ({ focused = false, color = "#000", size = 24 }) => {
-      const iconName = getTabIcon(route.name, focused);
-      return <Ionicons name={iconName} size={size} color={color} />;
-    },
-    tabBarActiveTintColor: "#000",
-    tabBarInactiveTintColor: "gray",
-    headerShown: false,
-  };
-}
-
-function Placeholder({ title = "" }) {
+function Placeholder({ route }) {
+ 
+  const title = route.params?.title || "Default";
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>{title}</Text>
     </View>
   );
 }
-
+ 
+const BottomTabs = createBottomTabNavigator({
+  screenOptions: ({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+      const iconName = getTabIcon(route.name, focused);
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+    tabBarActiveTintColor: "#000",
+    tabBarInactiveTintColor: "gray",
+    headerShown: false,
+  }),
+  screens: {
+    [TabOption.COUNTER]: {
+      screen: CounterHomeScreen,
+    },
+    [TabOption.ANALYTICS]: {
+      screen: Placeholder,
+      initialParams: { title: "Analytics" },  
+    },
+    [TabOption.GOALS]: {
+      screen: Placeholder,
+      initialParams: { title: "Goals" },
+    },
+    [TabOption.SETTINGS]: {
+      screen: Placeholder,
+      initialParams: { title: "Settings" },
+    },
+  },
+});
+ 
+export const Navigation = createStaticNavigation(BottomTabs);
+ 
 export default function TabNavigator() {
-  return (
-    <Tab.Navigator screenOptions={createScreenOptions}>
-      <Tab.Screen name={TabOption.COUNTER} component={CounterHomeScreen} />
-      <Tab.Screen
-        name={TabOption.ANALYTICS}
-        children={() => <Placeholder title="Analytics" />}
-      />
-      <Tab.Screen
-        name={TabOption.GOALS}
-        children={() => <Placeholder title="Goals" />}
-      />
-      <Tab.Screen
-        name={TabOption.SETTINGS}
-        children={() => <Placeholder title="Settings" />}
-      />
-    </Tab.Navigator>
-  );
+  return <Navigation />;
 }
